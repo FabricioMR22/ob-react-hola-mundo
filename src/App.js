a099574c653e82a50308fc4a3d112a70ae88de30
Raul Fabricio Martinez Ramos <u19218420@utp.edu.pe>
2022-11-15 00:50:33 -0500

    app: mostrando el taskListComponent; creando estructura de las task, creando componentes (tecnica)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/pure/greeting';
-import GreetingF from './components/pure/greetingF';
+/* import Greeting from './components/pure/greeting';
+import GreetingF from './components/pure/greetingF'; */
+import TaskListComponent from './components/container/task_list';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/* <Greeting name="Martin"></Greeting> */}
-        <GreetingF name="Martin"></GreetingF>
+        {/* <GreetingF name="Martin"></GreetingF> */}
+        <TaskListComponent></TaskListComponent>
       </header>
     </div>
   );
