218b6b2e9a24dfb08efceecb0610285d9913027d
Raul Fabricio Martinez Ramos <u19218420@utp.edu.pe>
2022-11-15 19:42:52 -0500

    Poniendo en practica ciclo de vida en Tasks el proyecto

--- src/components/pure/task.jsx
+++ src/components/pure/task.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Task } from '../../models/task.class';
 
 const TaskComponent = ({task}) => {
+
+    useEffect(() => {
+        console.log('Create task')
+        return () => {
+            console.log(`Task: ${task.name} is going to unmount`)
+        };
+    }, []);
+
     return (
         <div>
             <h2>
